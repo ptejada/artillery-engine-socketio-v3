b3c3ea7260173915eed86249092afe8c1cfbe7ed
Pablo Tejada <pablo@tejada.dev>
2023-05-28 00:24:41 -0400

    Adds additional tests cases

--- test-app/server.js
+++ test-app/server.js
@@ -12,7 +12,7 @@ io.on('connection', function(socket) {
   })
 });
 
-const tokens = []
+const tokens = ['all-access-token']
 io.of('secured').on('connection', function(socket) {
   socket.on('login', function({username, password}) {
     if (username === password) {
@@ -35,3 +35,43 @@ io.of('secured').on('connection', function(socket) {
     }
   })
 })
+
+io.of('inspect').on('connection', (socket) => {
+  socket.emit('result', {
+    headers: socket.request.headers,
+    query: socket.request.query
+  })
+})
+
+/*
+ * Secured namespace with middleware
+ */
+const securedMd = io.of('secured-md');
+
+// Middleware authenticating namespace
+securedMd.use((socket, next) => {
+  if (validAuth(socket.handshake.auth)) {
+    next()
+  } else {
+    const err = new Error("Not authorized");
+    err.data = { content: "Please retry later" }; // additional details
+    next(err);
+  }
+})
+securedMd.on('connection', function(socket) {
+  socket.on('ping', (pong) => pong(true))
+})
+
+function validAuth({token, type}) {
+  if (token) {
+    if (tokens.includes(token)) {
+      return true
+    }
+
+    if (type === 'uuid' && token.split('-').length === 5) {
+      return true
+    }
+  }
+
+  return false;
+}
